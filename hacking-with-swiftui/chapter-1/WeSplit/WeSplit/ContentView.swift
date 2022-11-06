9232695304c2d12bdc8184b65f51e91d7d16f956
Mehmet Tarhan <hi@memtarhan.com>
2022-11-06 02:22:14 +0000

    [Chapter 1] - @State property wrapper

--- hacking-with-swiftui/chapter-1/WeSplit/WeSplit/ContentView.swift
+++ hacking-with-swiftui/chapter-1/WeSplit/WeSplit/ContentView.swift
@@ -8,34 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            /// A Form can get up to 10 elements but Group can be used
-            VStack {
-                Form {
-                    Group {
-                        Text("A1")
-                        Text("A2")
-                    }
+    @State private var tapCount = 0
 
-                    Group {
-                        Text("B1")
-                        Text("B2")
-                    }
-                }
-                Form {
-                    Section {
-                        Text("A1")
-                        Text("A2")
-                    }
-                    Section {
-                        Text("B1")
-                        Text("B2")
-                    }
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.automatic)
+    var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
 }
