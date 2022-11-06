8f51ad09d6105e5a8cade43c61d33c8de419f839
Mehmet Tarhan <hi@memtarhan.com>
2022-11-06 13:55:53 +0000

    [Chapter 1] - Picker and list ForEach

--- hacking-with-swiftui/chapter-1/WeSplit/WeSplit/ContentView.swift
+++ hacking-with-swiftui/chapter-1/WeSplit/WeSplit/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
-    
-    /**
-     - Binding variables
-     -> $variableName : read and write to the same value, two-way binding
-     -> $variableName: The value of the property is read, and also written back when it's changed.
-     */
+//    @State private var name = ""
+//
+//    /**
+//     - Binding variables
+//     -> $variableName : read and write to the same value, two-way binding
+//     -> $variableName: The value of the property is read, and also written back when it's changed.
+//     */
+//    var body: some View {
+//        Form {
+//            TextField("Enter your name", text: $name)
+//            Text("Your name is \(name)")
+//        }
+//    }
+
+    // MARK: - Looping
+
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            Section {
+                ForEach(0 ..< 10) { number in
+                    Text("Row \(number)")
+                }
+            }
+            Section {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
     }
 }
