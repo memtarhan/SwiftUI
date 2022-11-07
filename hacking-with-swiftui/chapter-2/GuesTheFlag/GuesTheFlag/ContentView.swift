5a85e9604e232084ff829c3db4f533cf26599f2a
Mehmet Tarhan <hi@memtarhan.com>
2022-11-07 22:40:43 +0000

    [Chapter 2] - GuessTheFlag more customisation

--- hacking-with-swiftui/chapter-2/GuesTheFlag/GuesTheFlag/ContentView.swift
+++ hacking-with-swiftui/chapter-2/GuesTheFlag/GuesTheFlag/ContentView.swift
@@ -19,32 +19,49 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+            center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
 
-            VStack(spacing: 32) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            VStack {
+                Spacer()
 
-                VStack(spacing: 20) {
-                    ForEach(0 ..< 3) { number in
-                        Button {
-                            // flag was tapped
-                            flagTapped(number)
-                        } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                Text("Guess The Flag!")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+
+                VStack(spacing: 32) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+
+                    VStack(spacing: 16) {
+                        ForEach(0 ..< 3) { number in
+                            Button {
+                                // flag was tapped
+                                flagTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 5)
+                            }
                         }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
 
                 VStack {
                     Text("Score")
@@ -55,7 +72,10 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .font(.largeTitle.weight(.heavy))
                 }
+                
+                Spacer()
             }
+            .padding(20)
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
