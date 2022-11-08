59d9a59678e9d6fd8ac77d8423fd551eceac6bbd
Mehmet Tarhan <hi@memtarhan.com>
2022-11-08 22:36:49 +0000

    [Chapter 4] - Date example

--- hacking-with-swiftui/chapter-4/BetterRest/BetterRest/ContentView.swift
+++ hacking-with-swiftui/chapter-4/BetterRest/BetterRest/ContentView.swift
@@ -8,18 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
-
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
-            .padding()
-//            .labelsHidden()
-    }
-
-    var dateRange: ClosedRange<Date> {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now ... tomorrow
-        return range
+        Text("Hey")
     }
 }
 
