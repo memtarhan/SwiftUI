ba567345e9451586297d97966d6b7174da62f414
Mehmet Tarhan <hi@memtarhan.com>
2022-11-08 22:42:22 +0000

    [Build an App] - Created iDine app and added assets

---
+++ build-an-app/iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mehmet Tarhan on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
