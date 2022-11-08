7c8283aa8443423fb1a5aa17a8f73c7675e35eb7
Mehmet Tarhan <hi@memtarhan.com>
2022-11-08 23:43:32 +0000

    [Build an App] - Item details

---
+++ build-an-app/iDine/iDine/ItemDetail.swift
@@ -0,0 +1,42 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Mehmet Tarhan on 08/11/2022.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 10)
+                Text("Shot by \(item.photoCredit)")
+                    .padding(5)
+                    .background(.black.opacity(0.4))
+                    .font(.caption)
+                    .foregroundColor(.white)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+
+            Text(item.description)
+                .font(.body)
+            Spacer()
+        }
+        .padding(20)
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail(item: MenuItem.example)
+    }
+}
