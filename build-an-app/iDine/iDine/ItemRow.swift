d8f3594c09165d88b952c001c88e37cfa1c2b5bf
Mehmet Tarhan <hi@memtarhan.com>
2022-11-08 23:21:51 +0000

    [Build an App] - more customisations on row

--- build-an-app/iDine/iDine/ItemRow.swift
+++ build-an-app/iDine/iDine/ItemRow.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     let item: MenuItem
 
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
                 .clipShape(Circle())
+                .overlay(Circle().stroke(Color.primary, lineWidth: 1))
+            
             VStack(alignment: .leading) {
                 Text(item.name)
                     .font(.headline)
                 Text("$\(item.price)")
                     .font(.footnote)
+            }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction]?.opacity(0.5))
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
