e126ba569cda4278d40a45598b57719282cd7a11
Mehmet Tarhan <hi@memtarhan.com>
2022-11-19 20:57:57 +0000

    [Chapter 5] - Lists

---
+++ hacking-with-swiftui/chapter-5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Mehmet Tarhan on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
