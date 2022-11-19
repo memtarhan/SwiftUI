cff5a0082f6e6a75bfb840fedcf976ee9c5c7952
Mehmet Tarhan <hi@memtarhan.com>
2022-11-19 20:36:19 +0000

    [SwiftUI by Example] - Images

---
+++ swiftui-by-example/Images/Images/Images/ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesApp.swift
+//  Images
+//
+//  Created by Mehmet Tarhan on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
