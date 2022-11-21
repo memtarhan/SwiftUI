f3ebbe2281cc6986cf248887b5372fb0a0932c9c
Mehmet Tarhan <hi@memtarhan.com>
2022-11-21 11:21:46 +0000

    [Bucket List] - User struct, conforming to Comparable (sorting)

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.firstName < rhs.firstName
+    }
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Mehmet", lastName: "Tarhan"),
+        User(firstName: "Adem", lastName: "Tarhan"),
+        User(firstName: "Ali", lastName: "Tarhan"),
+    ].sorted()
+
     let values = [1, 5, 3, 4, 8, 9, 6, 2].sorted()
 
     var body: some View {
-        List(values, id: \.self) {
-            Text("\($0)")
+        VStack(spacing: 0) {
+            List(values, id: \.self) {
+                Text("\($0)")
+            }
+            List(users) { user in
+                Text("\(user.firstName) \(user.lastName)")
+            }
         }
     }
 }
