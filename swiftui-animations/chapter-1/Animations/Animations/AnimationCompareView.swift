0e7c968e488cfc632d102b10f12b17c059c2617b
Mehmet Tarhan <hi@memtarhan.com>
2025-03-20 12:35:26 +0000

    [SwiftUI Animations] - chapter 1 - starter project

--- swiftui-animations/chapter-1/Animations/Animations/AnimationCompareView.swift
+++ swiftui-animations/chapter-1/Animations/Animations/AnimationCompareView.swift
@@ -1,63 +1,71 @@
 import SwiftUI
 
 struct AnimationCompareView: View {
-  @State var animations: [AnimationData] = []
-  @State var location = 0.0
+    @State var animations: [AnimationData] = []
+    @State var location = 0.0
 
-  func deleteAnimations(at offsets: IndexSet) {
-    animations.remove(atOffsets: offsets)
-  }
-
-  func moveAnimations(source: IndexSet, destination: Int) {
-    animations.move(fromOffsets: source, toOffset: destination)
-  }
-
-  var body: some View {
-    NavigationStack {
-      VStack {
-        List {
-          ForEach($animations) { $animation in
-            NavigationLink {
-              EditAnimation(animation: $animation)
-            } label: {
-              VStack(alignment: .leading) {
-                Text(animation.description)
-                  .fixedSize(horizontal: false, vertical: true)
-                AnimationView(
-                  animation: animation,
-                  location: $location
-                )
-                .frame(height: 30)
-              }
-            }
-          }
-          .onDelete(perform: deleteAnimations)
-          .onMove(perform: moveAnimations)
-          Button {
-            let newAnimation = AnimationData(type: .linear)
-            animations.append(newAnimation)
-          } label: {
-            Label(
-              "Add Animation",
-              systemImage: "plus"
-            ).font(.title2)
-          }
-        }
-        .toolbar {
-          EditButton()
-        }
-        .navigationBarTitle("Animation Compare")
-      }
+    func deleteAnimations(at offsets: IndexSet) {
+        animations.remove(atOffsets: offsets)
     }
-  }
+
+    func moveAnimations(source: IndexSet, destination: Int) {
+        animations.move(fromOffsets: source, toOffset: destination)
+    }
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // 1
+                Button("Animate!") {
+                    // 2
+                    location = location == 0 ? 1 : 0
+                }
+                .font(.title)
+                .disabled(animations.isEmpty)
+                
+                List {
+                    ForEach($animations) { $animation in
+                        NavigationLink {
+                            EditAnimation(animation: $animation)
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text(animation.description)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                AnimationView(
+                                    animation: animation,
+                                    location: $location
+                                )
+                                .frame(height: 30)
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteAnimations)
+                    .onMove(perform: moveAnimations)
+                    Button {
+                        let newAnimation = AnimationData(type: .linear)
+                        animations.append(newAnimation)
+                    } label: {
+                        Label(
+                            "Add Animation",
+                            systemImage: "plus"
+                        ).font(.title2)
+                    }
+                }
+                .toolbar {
+                    EditButton()
+                }
+                .navigationBarTitle("Animation Compare")
+            }
+        }
+    }
 }
 
 struct AnimationCompareView_Previews: PreviewProvider {
-  static var previews: some View {
-    let animation1 = AnimationData(type: .linear)
-    let animation2 = AnimationData(type: .spring)
-    AnimationCompareView(
-      animations: [animation1, animation2]
-    )
-  }
+    static var previews: some View {
+        let animation1 = AnimationData(type: .linear)
+        let animation2 = AnimationData(type: .spring)
+        AnimationCompareView(
+            animations: [animation1, animation2]
+        )
+    }
 }
--- swiftui-animations/chapter-1/Animations/Animations/AnimationView.swift
+++ swiftui-animations/chapter-1/Animations/Animations/AnimationView.swift
@@ -1,25 +1,45 @@
 import SwiftUI
 
 struct AnimationView: View {
-  var animation: AnimationData
-  @Binding var location: Double
+    var animation: AnimationData
+    @Binding var location: Double
 
-  var body: some View {
-    GeometryReader { proxy in
-      Group {
-        Text("Animation")
-      }
+    var currentAnimation: Animation {
+        switch animation.type {
+        case .easeIn:
+            return Animation.easeIn(duration: animation.length)
+        case .easeOut:
+            return Animation.easeOut(duration: animation.length)
+        case .easeInOut:
+            return Animation.easeInOut(duration: animation.length)
+        default:
+            return Animation.linear(duration: animation.length)
+        }
     }
-  }
+
+    var body: some View {
+        GeometryReader { proxy in
+            Group {
+                HStack {
+                    Image(systemName: "gear.circle")
+                        .rotationEffect(.degrees(360 * location))
+                    Image(systemName: "star.fill")
+                        .offset(x: proxy.size.width * location * 0.8)
+                }
+                .font(.title)
+                .animation(currentAnimation, value: location)
+            }
+        }
+    }
 }
 
 struct AnimationView_Previews: PreviewProvider {
-  static var previews: some View {
-    let animation = AnimationData(type: .linear, length: 1.0, delay: 0.0)
+    static var previews: some View {
+        let animation = AnimationData(type: .linear, length: 1.0, delay: 0.0)
 
-    AnimationView(
-      animation: animation,
-      location: .constant(0.0)
-    )
-  }
+        AnimationView(
+            animation: animation,
+            location: .constant(0.0)
+        )
+    }
 }
